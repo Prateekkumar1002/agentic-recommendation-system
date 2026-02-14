0d9ef56ceeb49c7881760d2f4b052f581c982257
Prateek Kumar <63146429+Prateekkumar1002@users.noreply.github.com>
2026-02-14 21:58:02 +0530

    Update optimizer_agent.py

--- agentic-recsys/agents/optimizer_agent.py
+++ agentic-recsys/agents/optimizer_agent.py
@@ -1,8 +1,9 @@
 import random
-
+#Optimizer Agent
 class OptimizerAgent:
     def suggest(self):
         return {
             "learning_rate": random.choice([0.01, 0.05, 0.1]),
             "n_estimators": random.choice([100, 200, 300])
         }
+
