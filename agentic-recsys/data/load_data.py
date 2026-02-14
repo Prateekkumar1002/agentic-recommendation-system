60afb5c5df8093e8acc572cd262fa92b6ee0df23
Prateek Kumar <63146429+Prateekkumar1002@users.noreply.github.com>
2026-02-14 21:59:27 +0530

    Update load_data.py

--- agentic-recsys/data/load_data.py
+++ agentic-recsys/data/load_data.py
@@ -1,7 +1,8 @@
 import pandas as pd
-
+#Load data of users, items and interactions
 def load_data():
     users = pd.read_csv("data/users.csv")
     items = pd.read_csv("data/items.csv")
     interactions = pd.read_csv("data/interactions.csv")
     return users, items, interactions
+
